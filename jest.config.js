0985648d5573f350bd2a2c0e5337e9ca7b9d6901
salilkapoor <salilkapoor03@gmail.com>
2020-05-22 09:58:54 +0530

    test(config): updated jest config

--- jest.config.js
+++ jest.config.js
@@ -4,19 +4,44 @@ module.exports = {
     '!**/*.d.ts',
     '!**/node_modules/**'
   ],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '<rootDir>/coverage',
+    '<rootDir>/jest.config.js',
+    '<rootDir>/next.config.js',
+    '<rootDir>/server.js',
+    '<rootDir>/pages'
+  ],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
-  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/coverage/', '/jest.config.js/', '/next.config.js/', '/server.js/'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/.next/',
+    '<rootDir>/coverage',
+    '<rootDir>/jest.config.js',
+    '<rootDir>/next.config.js',
+    '<rootDir>/server.js'
+  ],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
   },
-  transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
-  coverageReporters: ['text', 'json', 'lcov', 'json-summary', 'text-summary'],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$'
+  ],
+  coverageReporters: [
+    'text',
+    'json',
+    'lcov',
+    'json-summary',
+    'text-summary'
+  ],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 3,
-      lines: 3,
-      statements: 0.7
+      branches: 70,
+      functions: 60,
+      lines: 70,
+      statements: 75
     }
   }
 };
